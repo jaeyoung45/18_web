be7300945d74ed9d9912d28f0edda7410214b6dc
it <it@DESKTOP-5HJRIQD>
2018-11-26 11:47:14 +0900

    친구

---
+++ Friend/src/FriendWrite/Friend.java
@@ -0,0 +1,45 @@
+package FriendWrite;
+
+public class Friend {
+	private int Num;
+	private String Name;
+	private String Birth;
+	private String Phone;
+	private String Addr;
+	
+	
+	public int getNum() {
+		return Num;
+	}
+	public String getName() {
+		return Name;
+	}
+	public String getBirth() {
+		return Birth;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public String getAddr() {
+		return Addr;
+	}
+	public void setNum(int num) {
+		Num = num;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public void setBirth(String birth) {
+		Birth = birth;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public void setAddr(String addr) {
+		Addr = addr;
+	}
+	
+	
+
+
+}
